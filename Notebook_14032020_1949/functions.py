840d031ea33aec201ef2123825443ee8fc1b3c99
Maurras TOGBE <maurrastogbe@yahoo.fr>
2020-03-24 18:20:26 +0100

    MUT 14032020
    Create functions for simple executions

--- Notebook_14032020_1949/functions.py
+++ Notebook_14032020_1949/functions.py
@@ -37,25 +37,20 @@ class Comparison:
   #It is the function to call when we want to test differents models with differents values for parameters
   def run_comparison(data, window = 100, estimators = 50, anomaly = 0.5, drift_rate = 0.3, output_file = 'results'):
       
-      # = data = SEAGenerator(classification_function=0, noise_percentage=0.7, random_state=1)
+    # = data = SEAGenerator(classification_function=0, noise_percentage=0.7, random_state=1)
     models = [HalfSpaceTrees(n_features=stream.n_features, window_size=window, 
-    n_estimators=estimators, 
-    size_limit=0.1*100, 
-    anomaly_threshold=anomaly,
-    depth=15, 
-    random_state=2),
-    
-    IsolationForestStream(
-    window_size=window, n_estimators=estimators,
-     anomaly_threshold=anomaly, 
-     drift_threshold=drift_rate,
-      random_state=None)]
-    
-    
-      # Setup the evaluator
-      evaluator = EvaluatePrequential(pretrain_size=1, max_samples=1000, show_plot=True, 
-      metrics=['accuracy', 'f1', 'kappa', 'kappa_m', 'running_time','model_size'], 
-      batch_size=1, output_file = 'results_test.csv')
-      # 4. Run the evaluation
-      evaluator.evaluate(stream=stream, model=models, model_names=['HSTrees','iForestASD'])
-      return 
+                             n_estimators=estimators, size_limit=0.1*100, 
+                             anomaly_threshold=anomaly, depth=15, 
+                             random_state=2),
+    IsolationForestStream(window_size=window, n_estimators=estimators, 
+                          anomaly_threshold=anomaly, drift_threshold=drift_rate, 
+                          random_state=None)]
+    # Setup the evaluator
+    evaluator = EvaluatePrequential(pretrain_size=1, max_samples=1000, 
+                                    show_plot=True, 
+                                    metrics=['accuracy', 'f1', 'kappa', 'kappa_m', 
+                                             'running_time','model_size'], 
+                                             batch_size=1, output_file = 'results_test.csv') 
+    # 4. Run the evaluation 
+    evaluator.evaluate(stream=stream, model=models, model_names=['HSTrees','iForestASD'])
+    return 
